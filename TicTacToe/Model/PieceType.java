fd33ee2ec7af529752b9e317c53ef4ff1fa43021
Abhishek verma <41055342+abhishekabs17@users.noreply.github.com>
2025-09-18 12:12:26 +0530

    Delete TicTacToe/Model directory

--- TicTacToe/Model/PieceType.java
+++
@@ -1,6 +0,0 @@
-package com.conceptandcoding.LowLevelDesign.LLDTicTacToe.Model;
-
-public enum PieceType {
-    X,
-    O;
-}
--- TicTacToe/Model/PlayingPieceX.java
+++
@@ -1,8 +0,0 @@
-package com.conceptandcoding.LowLevelDesign.LLDTicTacToe.Model;
-
-public class PlayingPieceX extends PlayingPiece{
-
-    public PlayingPieceX() {
-        super(PieceType.X);
-    }
-}
